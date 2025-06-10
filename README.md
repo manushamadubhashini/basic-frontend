0d7aadb95199f4509df787770236d4a6e69039dc
Acer <madubhashinimanusha@gmail.com>
2025-06-10 22:30:05 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Our First Project - Frontend Basic
+
+## Initial Setup
+
+1.Code Editor
+
+  - VS Code
+
+2.Build Tools (Enviroment Tools)
+
+  - Node.js  
+  - npm
+
+## Available Script
+
+    1.npm start  
+    2.npm run dev
+
+## References
+
+  - [flatuicolors.com](https://flatuicolors.com/)
+
---
+++ script.js
@@ -0,0 +1,18 @@
+// External JavaScript
+
+var userEmail = "manusha@gmail.com"
+const userName = "manusha madubhashini"
+let userAge =  21;
+
+// userName = "manushi"
+
+// This is a single line comment
+
+/*
+ This is a
+ multiline
+ comment
+ */
+
+userAge =22;
+console.log("My First JavaScript File is Here! ",{userEmail,userName,userAge});
