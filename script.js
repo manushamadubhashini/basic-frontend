1b5ec3cc486739c9d520166a2c6e7fe3a99c80fa
Acer <madubhashinimanusha@gmail.com>
2025-07-04 17:35:59 +0530

    define javaScript varibale type

--- script.js
+++ script.js
@@ -3,6 +3,19 @@
 var userEmail = "manusha@gmail.com"
 const userName = "manusha madubhashini"
 let userAge =  21;
+const isAuthenticated = false
+const lastName = null
+const firstName = undefined
+const userData = {
+    firstName : "Manusha",
+    lastName : "Madubhashini",
+    age : 21
+}
+// Arrays
+const fruits = ["banana","apple","orange"] // String Array
+const numbers = [1,2,3,4] // Number Array
+const users = [{id :"S001",name:"Manusha"},{id:"S002",name:"Nimesha"}] // Object Array
+
 
 // userName = "manushi"
 
@@ -16,3 +29,12 @@ let userAge =  21;
 
 userAge =22;
 console.log("My First JavaScript File is Here! ",{userEmail,userName,userAge});
+console.log(userName,typeof userName)
+console.log(userAge,typeof userAge)
+console.log(isAuthenticated , typeof isAuthenticated)
+console.log(lastName, typeof lastName)
+console.log(firstName,typeof firstName)
+console.log("value:",userData,typeof userData)
+console.log("value:",fruits,typeof fruits)
+console.log("value:",numbers,typeof numbers)
+console.log("value:",users,typeof users)
