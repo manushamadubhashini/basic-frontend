a0f8bf8cdba1bd437b72285d8fc8a4fc71b8127e
Acer <madubhashinimanusha@gmail.com>
2025-07-14 22:15:49 +0530

    handle synchronous request

--- Day - 04/eventloop.js
+++ Day - 04/eventloop.js
@@ -1,1 +1,34 @@
 console.log("event loop")
+let  promise = new Promise((resolve, reject) => {
+    let success = true
+    if(success){
+        setTimeout(() => resolve("promise resolved"),3000)
+    }else{
+        reject("promise rejected!")
+    }
+});
+
+async function initiateApp(){
+    try{
+        const message = await promise
+        console.log("message from promise: ",message)
+
+        const userResponse = await fetch("https://jsonplaceholder.typicode.com/users/1")
+        const userData = await userResponse.json()
+
+        if(userData ?.id){
+            console.log("user1 : ",userData)
+            const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userData.id}`)
+            const postUser = await postResponse.json()
+            console.log("post: ",postResponse)
+        }
+
+    }catch(error){
+        console.log("Error fetching data: ",error)
+    }
+}
+// long running task
+initiateApp() // call the async functions
+
+// show running task
+console.log("I'm here after async functions")
